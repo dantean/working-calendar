1ed9dc60172ff6318b64bd0c1c5014778c6081eb
Joseph Monticelli <jdmonticelli@gmail.com>
2024-04-28 15:00:52 -0400

    added comments to HTML and JS documents

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,26 @@
+// Wraps all code in a function that executes once the document is finished loading, preventing certain errors
+
 $(function () {
 
+  // variables that determine current day and hour
 var currentDay = dayjs();
 var currentHour = dayjs().hour();
+
+// displays and formats the current day
 var currentDayEl=$("#currentDay");
 var saveBtnEl = $(".saveBtn");
 currentDayEl.text(currentDay.format("dddd, MMM DD"));
 
+// 'for loop' that goes through the hours of the day
 for(i=9; i < 18; i++) {
   parentId = $("#hour-"+i);
   textArea = parentId.find("textarea"); // .find should select the correct element more reliably than .children
-  var loadEvent = localStorage.getItem("hour-"+i);
+// loads saved events from local storage
 
-  textArea.val(loadEvent);
+  var loadEvent = localStorage.getItem("hour-"+i);
+    textArea.val(loadEvent);
+
+  // changes CSS classes based on the hour for "present", "past", and "future" 
   if(i === currentHour) {
     textArea.addClass("present");
   } else if (i < currentHour) {
@@ -20,6 +29,8 @@ for(i=9; i < 18; i++) {
     textArea.addClass("future");
   } 
 }
+
+// event listener for the 'save' feature
 $(".container-lg").on("click", ".saveBtn", function(event) {
   var button = $(this);  
   var textAreaEl = button.siblings("textarea");  
@@ -27,6 +38,7 @@ $(".container-lg").on("click", ".saveBtn", function(event) {
   localStorage.setItem(parentId, textAreaEl.val()); 
 });
 
+// I added this just to confirm that everything is fully loaded :)
 console.log("~all elements are now loaded~");
 });
   
